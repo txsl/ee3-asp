7362c22efc5c60662b92eee9d40fdfddd463e198
Thomas <txl11@imperial.ac.uk>
2014-04-06 20:11:18 +0100

    Part 3 (2.3) of 2.2 done

--- Part_2/Part_2_2.m
+++ Part_2/Part_2_2.m
@@ -2,7 +2,15 @@ clear all; close all;
 
 x = randn(1, 1000);
 y = filter(ones(9,1),[1],x);
+ycorr = xcorr(y, 'unbiased');
 figure;
-stem(xcorr(y, 'unbiased'))
-zoom xon
-zoom(50)
+stem(-20:20, ycorr(980:1020))
+
+% Part 3
+
+xycorr = xcorr(x, y, 'unbiased');
+
+figure;
+stem(-20:20, xycorr(980:1020))
+figure;
+stem(xycorr)
