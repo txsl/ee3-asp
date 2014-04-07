8519be45e27abc9a0d0d79c94479cfa1ee6429cb
Thomas Lim <txl11@ic.ac.uk>
2014-04-07 03:04:45 +0100

    We've got most of part 1 locked down properly (and written up)

--- Part_1/ASP_Part_1_1.m
+++ Part_1/ASP_Part_1_1.m
@@ -1,52 +1,87 @@
-close all;
-clear;
+close all; clear; clc;
 
-X = rand(1000, 1);
-% scatter(1:1000, x)
-xlabel('Sample Number')
-ylabel('Value')
+%% 1.1
 
-m = mean(X(:,1))
-s = std(X(:,1))
 
+X1 = rand(1000, 1);
+
+% Calculating our 1000 sample mean and std dev for 1.1.1 and 1.1.2
+m1 = mean(X1);
+s1 = std(X1);
+
+
+%% 1.1.3
+% Ensemble of 10 1000 sample sets
 X = rand(1000, 10);
 
-a = rand(1000,10);
-m = zeros(10,1);
-s = zeros(10,1);
+% To get non SI x labels
+%set(gca, 'XTickLabel', num2str(get(gca, 'XTick'))) 
 
-
-m = mean(a);
-s = std(a);
-
-B = 0.5 - m
+m10 = mean(X);
+s10 = std(X);
 
 figure;
-bar(1:10, m);
-title('Mean')
-hlin = refline(0,0.5)
-set(hlin,'Color','r')
-figure;
-bar(1:10, s);
-title('Standard Deviation');
+subplot(1,2,1)
+stem(1:10, 0.5-m10);
+title('Mean Bias');
+xlabel('Ensemble Iteration');
+ylabel('Bias');
 
-figure;
+subplot(1,2,2)
+stem(1:10, s10- 0.2887);
+title('Standard Deviation Bias');
+xlabel('Ensemble Iteration');
+ylabel('Bias');
+
+%% 1.1.4
+
+% The hist function puts the data in to bins
 a = hist(X(:,1));
-a = a/1000
-bar(a)
+
+% Normalise the histogram based on our sample size (to create a p.d.f.)
+a = a/1000;
 
 figure;
-a = hist(X(:,1), 100);
-a = a/1000
-bar(a)
-xlim([0 101])
+bar([0.1:0.1:1] , a)
+xlim([0 1.1])
+hline = refline([0 0.1]);
+set(hline,'Color','r')
+title('pdf of 1000 samples, in 10 bins');
+xlabel('x');
+ylabel('Density');
+legend('Actual pdf', 'Theoretical pdf');
 
+
+% This time we are splitting the pdf in to 50 bins
+a = hist(X1(:,1), 50);
+a = a/1000;
+
+figure;
+bar([0.02:0.02:1], a);
+xlim([0 1.02]);
+hline = refline([0 0.02]);
+set(hline,'Color','r')
+title('pdf of 1000 samples, in 50 bins');
+xlabel('x');
+ylabel('Density');
+legend('Actual pdf', 'Theoretical pdf');
+
+
+% This time, we are just using 100 samples
 figure
 a = hist(X(1:100,1), 10);
-a = a/100
-bar(a)
+a = a/100; % Normalised by 100 samples this time
+bar([0.02:0.02:1], a);
+xlim([0 1.02]);
+hline = refline([0 0.1]);
+set(hline,'Color','r')
+title('pdf of 100 samples, in 10 bins');
+xlabel('x');
+ylabel('Density');
+legend('Actual pdf', 'Theoretical pdf');
 
 
+%% 1.1.5 
 
 X = randn(1000, 1);
 mean(X)
