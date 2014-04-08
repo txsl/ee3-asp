f83fe0a5716d4f60a6e9522631bf7e9460b4d4c4
Thomas <txl11@imperial.ac.uk>
2014-04-08 12:28:08 +0100

    2.4.1 is looking pretty neat now

--- Part_2/Part_2_4.m
+++ Part_2/Part_2_4.m
@@ -1,10 +1,14 @@
 clear all; close all;
 
+%% 2.4.1
+
 a1 = -2.5 + 5*rand(1000,1);
 a2 = -1.5 + 3*rand(1000,1);
 
 a1stable = [];
 a2stable = [];
+a1unstable = [];
+a2unstable = [];
 
 for i = 1:length(a1)
     w = randn(1000, 1);
@@ -17,40 +21,61 @@ for i = 1:length(a1)
     if(abs(x(1000)) < 10)
         a1stable = [a1stable a1(i)];
         a2stable = [a2stable a2(i)];
+    else
+        a1unstable = [a1unstable a1(i)];
+        a2unstable = [a2unstable a2(i)];
     end
 end
+subplot (1,2,1);
+plot(a1stable, a2stable, '^r')
+hold on;
+plot(a1unstable, a2unstable, '+')
+hold off;
+legend('Convergent', 'Non-Convergent');
+xlabel('a1');
+ylabel('a2');
+title('Randomly generated processes to show the stability triangle');
 
-plot(a1stable, a2stable, '*')
 
-
-
-% a1 = -2.5:0.1:2.5;
+%  We don't need to define a1 as we are using it build in to the for loop
 a2 = -1.5:0.1:1.5;
 
 a1stable = [];
 a2stable = [];
+a1unstable = [];
+a2unstable = [];
 
-for i = -2.5:0.1:2.5
+for a1 = -2.5:0.1:2.5
     for k = 1:length(a2)
         w = randn(1000, 1);
         x = zeros(1000, 1);
         x(1) = w(1);
         x(2) = w(2);
         for j = 3:1000
-            x(j) = i*x(j-1) + a2(k)*x(j-2);
+            x(j) = a1*x(j-1) + a2(k)*x(j-2);
         end
         if(abs(x(1000)) < 10)
-            a1stable = [a1stable i];
+            a1stable = [a1stable a1];
             a2stable = [a2stable a2(k)];
+        else
+            a1unstable = [a1unstable a1];
+            a2unstable = [a2unstable a2(k)];
         end
     end
 end
 
-figure;
-plot(a1stable, a2stable, '*')
 
+subplot (1,2,2);
+plot(a1stable, a2stable, '^r')
+hold on;
+plot(a1unstable, a2unstable, '+')
+hold off;
+legend('Convergent', 'Non-Convergent');
+xlabel('a1');
+ylabel('a2');
+title('Series generated processes to show the stability triangle');
 
-% 2.4.2 Sunspot Models
+%% 2.4.2 Sunspot Models
 load('sunspot.dat')
 N5 = xcorr(sunspot(50:55, 2));
 figure;
