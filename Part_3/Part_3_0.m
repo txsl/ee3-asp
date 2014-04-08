cd179691a24e9145e7da55ecbacaf258a466b5aa
Thomas <txl11@imperial.ac.uk>
2014-04-08 23:21:32 +0100

    3.0 done

--- Part_3/Part_3_0.m
+++ Part_3/Part_3_0.m
@@ -1,16 +1,29 @@
 close all; clear; clc;
 
+subplot(1,3,1);
+
 x = randn(1, 128);
 p = pgm(x);
 stem(p)
 xlim([0 128]);
+xlabel('Frequency Bin');
+ylabel('Magnitude');
+title('PSD of WGN, N=128');
 
+subplot(1,3,2);
 x = randn(1, 256);
 p = pgm(x);
 stem(p)
 xlim([0 256]);
+xlabel('Frequency Bin');
+ylabel('Magnitude');
+title('PSD of WGN, N=256');
 
+subplot(1,3,3);
 x = randn(1, 512);
 p = pgm(x);
 stem(p)
 xlim ([0 512]);
+xlabel('Frequency Bin');
+ylabel('Magnitude');
+title('PSD of WGN, N=512');
