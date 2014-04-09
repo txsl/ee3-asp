c1a69cc99591059b8a3724d424e84f973b44144e
Thomas <txl11@imperial.ac.uk>
2014-04-09 23:03:09 +0100

    3.3 done

--- Part_3/Part_3_3.m
+++ Part_3/Part_3_3.m
@@ -1,4 +1,5 @@
 close all; clear; clc;
+path(path, '../voicebox');
 
 
 %% 3.3.1
@@ -50,20 +51,83 @@ plot((1:length(dial))/Fsamp, dial)
     xlabel('Time (seconds)');
     ylabel('Amplitude');
     title('London DMTF Number being dialled');
+    grid on;
     
 figure;
-subplot(1,2,1);
+subplot(2,1,1);
 plot((1:length(dial))/Fsamp, dial)
     xlim([0 5.25])
     xlabel('Time (seconds)');
     ylabel('Amplitude');
     title('0 dialtone, then deadtime');
-    xlim([.23 .255])
+    xlim([.23 .255]);
+    grid on;
     
-subplot(1,2,2);
+subplot(2,1,2);
 plot((1:length(dial))/Fsamp, dial)
     xlim([0 5.25])
     xlabel('Time (seconds)');
     ylabel('Amplitude');
     title('Deadtime, then 5 dialtone');
-    xlim([1.99 2.03])
+    xlim([1.99 2.01]);
+    grid on;
+  
+    
+%% 3.3.2
+
+figure;
+spgrambw(dial, Fsamp, 'pJcwD', 200, 2000, 20);
+
+% figure;
+% [t, f, p] = spgrambw(dial(1:N), Fsamp, 'pJcw', 200, 2000, 20);
+
+figure;
+% plot(f, p)
+
+y0 = fft(dial(1:N), N)/N;
+y2 = fft(dial(N+1:2*N), N)/N;
+
+f = ((Fsamp)*(1:N))/N;
+
+plot(f, abs(y0), 'r', f, abs(y2));
+    grid on;
+    xlim([0 2500]);
+    xlabel('Frequency [Hz]');
+    ylabel('Magnitude');
+    title('FFT of the 0 and 2 DTMF tone');
+    legend('0 Spectra', '2 Spectra');
+    
+    
+    
+%% 3.3.4
+
+% length of the entire number being dialled
+len = length(dial);
+var_coeffs = [.5 1.2 3];
+clean_dial = dial;
+
+
+for i = 1:3
+    dial = clean_dial;
+    dial = dial + var_coeffs(i)*randn(1, len);
+    
+    subplot(2,3,i)
+        spgrambw(dial, Fsamp, 'pJcwD', 200, 2000, 20);
+        t = sprintf('Additional Noise \\sigma_1= %f',var_coeffs(i));
+        title(t);
+    
+    y0 = fft(dial(1:N), N)/N;
+    y2 = fft(dial(N+1:2*N), N)/N;
+    
+    subplot(2,3,i+3)
+    plot(f, abs(y0), 'r', f, abs(y2));
+        grid on;
+        xlim([0 2500]);
+        xlabel('Frequency [Hz]');
+        ylabel('Magnitude');
+        title('FFT of the 0 and 2 DTMF tone');
+        legend('0 Spectra', '2 Spectra');
+    
+end
+sound(dial, Fsamp);
+
