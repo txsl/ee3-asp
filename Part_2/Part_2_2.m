fc341938b28860c111c60da0ad2eacadaf456ef5
Thomas Lim <txl11@ic.ac.uk>
2014-04-11 18:41:47 +0100

    2.3 done (in 2.2)

--- Part_2/Part_2_2.m
+++ Part_2/Part_2_2.m
@@ -17,3 +17,31 @@ stem(-20:20, ycorr(980:1020));
     ylabel('Correlation');
     title('ACF of 1000 order 9 MA filtered WGN process');
     grid on;
+
+    
+%% 2.3.1
+
+% Now let's cross correlate x and y
+xycorr = xcorr(x, y, 'unbiased');
+
+% Let's flip the processes around to see if it changes anything
+yxcorr = xcorr(y, x, 'unbiased');
+
+figure;
+subplot(1,2,1);
+
+% plot them
+stem(-20:20, xycorr(980:1020))
+    xlabel('Sample lag \tau');
+    ylabel('Correlation');
+    title('Cross correlation of filtered and unfiltered xy process');
+    grid on;
+    
+    
+subplot(1,2,2);
+stem(-20:20, yxcorr(980:1020))
+    xlabel('Sample lag \tau');
+    ylabel('Correlation');
+    title('Cross correlation of filtered and unfiltered yx process');
+    grid on;
+    
