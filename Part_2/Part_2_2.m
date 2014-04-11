b60541badf04c348b0ce261736c52a6de469b9f5
Thomas Lim <txl11@ic.ac.uk>
2014-04-11 16:12:04 +0100

    2.2 done

--- Part_2/Part_2_2.m
+++ Part_2/Part_2_2.m
@@ -1,16 +1,19 @@
 clear all; close all; clc;
 
+%% 2.2.1
+
+% Generate our WGN
 x = randn(1, 1000);
-y = filter(ones(9,1),[1],x);
+
+% Filter with an order 9 MA filter
+y = filter(ones(9,1),1,x);
+
+% ACF
 ycorr = xcorr(y, 'unbiased');
-figure;
-stem(-20:20, ycorr(980:1020))
 
-% Part 3
-
-xycorr = xcorr(x, y, 'unbiased');
-
-figure;
-stem(-20:20, xycorr(980:1020))
-figure;
-stem(xycorr)
+% Plot the results
+stem(-20:20, ycorr(980:1020));
+    xlabel('Sample lag \tau');
+    ylabel('Correlation');
+    title('ACF of 1000 order 9 MA filtered WGN process');
+    grid on;
