dd6e0c0c82064d25660ca31384514c20f72034d9
Thomas Lim <txl11@ic.ac.uk>
2014-04-13 00:29:55 +0100

    pgm function commented

--- Part_3/pgm.m
+++ Part_3/pgm.m
@@ -1,17 +1,31 @@
 function [ px ] = pgm( x )
-%PGM Summary of this function goes here
-%   Detailed explanation goes here
 
+% First get the length of our signal
 N = length(x);
-frac = ((1:N)/N).';
 
+% This defines the series from 0/n, 1/n ... N-1/n used the the summing sequence 
+% It's also transposed to set cur_exp in the correct dimension further down
+% (which is in turn used to take an inner product).
+frac = ((0:N-1)/N).';
+
+% We prepare the matrix of results
 px = zeros(N,1);
 
 for i = 1:N
+    
+    % the frequency scales from 0 to N-1 (as a fraction)
     f = i-1/N;
+    
+    % we calculate a matrix of the coefficients from the frequency scaling
     cur_exp = exp(-1i*2*pi*f*frac);
+    
+    % Then multiplied with our signal, in an inner product multiplication
     sum = x*cur_exp;
+    
+    % Take the magnitude, and square the result
     res = (abs(sum))^2/N;
+    
+    % Save the result to the correct frequency bin
     px(i) = res;
 end
 
